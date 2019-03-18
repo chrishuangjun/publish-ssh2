3071e42042c58de325f78905ad2a30d789a4156f
黄军 <jun9.huang@midea.com>
2019-03-18 16:57:57 +0800

    添加--help 命令说明

--- bin/index.js
+++ bin/index.js
@@ -14,31 +14,50 @@ commander.version(require('../package').version)
 commander.usage('<command>')
 
 commander
-	.command('publish')
-	.description('发布到服务器')
-	.alias('p')
-	.action(() => {
-		require('../command/publish')()
-	})
+    .command('publish')
+    .description('发布到服务器')
+    .alias('p')
+    .action(() => {
+        require('../command/publish')()
+    })
 
 commander
-	.command('generator_config')
-	.description('生成配置文件')
-	.alias('gcfg')
-	.action(() => {
-		require('../command/generator_config')()
-	})
+    .command('generator_config')
+    .description('生成配置文件')
+    .alias('gcfg')
+    .action(() => {
+        require('../command/generator_config')()
+    })
 
 commander
-	.command('revert')
-	.description('恢复至上个版本')
-	.alias('rv')
-	.action(() => {
-		require('../command/revert')()
-	})
+    .command('revert')
+    .description('恢复至上个版本')
+    .alias('rv')
+    .action(() => {
+        require('../command/revert')()
+    })
 
 // 错误命令时的显示帮助
 commander.parse(process.argv)
 if (!commander.args.length) {
-	commander.help()
+    commander.help()
 }
+
+const help = function () {
+    console.log()
+    console.log(chalk.red('  Examples:'))
+    console.log()
+    console.log(chalk.gray('    生成部署配置文件,命令如下：'))
+    console.log()
+    console.log(chalk.green('    publish-ssh2-cli gcfg'))
+    console.log()
+    console.log(chalk.grey('    根据部署配置文件说明填写信息，然后部署代码，命令如下：'))
+    console.log()
+    console.log(chalk.green('    publish-ssh2-cli p '))
+    console.log()
+    console.log(chalk.grey('    部署完成后，发现有问题，马上可以通过命令恢复至上个版本，命令如下：'))
+    console.log()
+    console.log(chalk.green('    publish-ssh2-cli rv'))
+    console.log()
+}
+commander.on('--help', help)
