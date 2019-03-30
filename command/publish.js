2eb885a748684fc564db4315415d79b6860eb614
黄军 <jun9.huang@midea.com>
2019-03-30 13:41:07 +0800

    bug fixed

--- command/publish.js
+++ command/publish.js
@@ -7,45 +7,45 @@ const { UploadDir } = require('./utils/sshUtils.js')
 const { readFile, fileDirExists } = require('./utils/fileUtils')
 const produceZip = require('./utils/produce-zip')
 
-module.exports = (cfgPath='./publishcfg/config.json') => {
-  co(function*() {
-    const fileExist = yield fileDirExists(cfgPath)
+module.exports = (cfgPath = 'publishcfg\\config.json') => {
+    co(function* () {
+        const fileExist = yield fileDirExists(cfgPath)
 
-    if (!fileExist) {
-      console.log(
-        chalk.red(
-          '\n发布失败，原因：请在项目根目录执行当前命令，并在config/config.json里面配置了server服务器信息。'
-        )
-      )
-      process.exit()
-    }
-    try {
-      const cfg = JSON.parse(yield readFile(cfgPath))
-      console.log(chalk.blue('服务器信息:'))
-      console.log(cfg.servers)
+        if (!fileExist) {
+            console.log(
+                chalk.red(
+                    '\n发布失败，原因：请在项目根目录执行当前命令，并在config/config.json里面配置了server服务器信息。'
+                )
+            )
+            process.exit()
+        }
+        try {
+            const cfg = JSON.parse(yield readFile(cfgPath))
+            console.log(chalk.blue('服务器信息:'))
+            console.log(cfg.servers)
 
-      const publishAnswer = yield prompt('是否发布（yes/no): ')
+            const publishAnswer = yield prompt('是否发布（yes/no): ')
 
-      if (publishAnswer === 'yes') {
-        console.log(chalk.blue('zip压缩中...'))
-        const cwd = process.cwd()
-        produceZip(
-          path.join(cwd, `${cfg.zipSavePath}/${cfg.zipName}.zip`),
-          path.join(cwd, cfg.localDir),
-          () => {
-            console.log(chalk.blue('zip压缩完成,发布中...'))
-            cfg.servers.forEach(server => {
-              UploadDir(server, cfg.zipName, cfg.zipSavePath, res => {
+            if (publishAnswer === 'yes') {
+                console.log(chalk.blue('zip压缩中...'))
+                const cwd = process.cwd()
+                produceZip(
+                    path.join(cwd, `${cfg.zipSavePath}/${cfg.zipName}.zip`),
+                    path.join(cwd, cfg.localDir),
+                    () => {
+                        console.log(chalk.blue('zip压缩完成,发布中...'))
+                        cfg.servers.forEach(server => {
+                            UploadDir(server, cfg.zipName, cfg.zipSavePath, res => {
+                                process.exit()
+                            })
+                        })
+                    }
+                )
+            } else {
                 process.exit()
-              })
-            })
-          }
-        )
-      } else {
-        process.exit()
-      }
-    } catch (ex) {
-      console.log(chalk.red(ex))
-    }
-  })
+            }
+        } catch (ex) {
+            console.log(chalk.red(ex))
+        }
+    })
 }
--- command/revert.js
+++ command/revert.js
@@ -3,49 +3,49 @@ const co = require('co')
 const chalk = require('chalk')
 const { readFile, fileDirExists } = require('./utils/fileUtils')
 function revert(config, zipName) {
-	const conn = new Client()
-	conn
-		.on('ready', function () {
-			conn.exec(
-				`rm -rf ${config.remoteDir}/${zipName}.zip && mv ${config.remoteDir}/${zipName}.zip.bak ${config.remoteDir}/${zipName}.zip && unzip -o ${zipName}.zip`,
-				function (err, stream) {
-					if (err) {
-						console.log(err)
-					}
-					stream
-						.on('close', function () {
-							conn.end()
-						})
-						.on('data', function () { })
-				}
-			)
-		})
-		.connect({
-			host: config.host,
-			port: config.port,
-			username: config.user,
-			password: config.password
-		})
+    const conn = new Client()
+    conn
+        .on('ready', function () {
+            conn.exec(
+                `rm -rf ${config.remoteDir}/${zipName}.zip && mv ${config.remoteDir}/${zipName}.zip.bak ${config.remoteDir}/${zipName}.zip && unzip -o ${zipName}.zip`,
+                function (err, stream) {
+                    if (err) {
+                        console.log(err)
+                    }
+                    stream
+                        .on('close', function () {
+                            conn.end()
+                        })
+                        .on('data', function () { })
+                }
+            )
+        })
+        .connect({
+            host: config.host,
+            port: config.port,
+            username: config.user,
+            password: config.password
+        })
 }
-module.exports = function (cfgPath) {
-	co(function* () {
-		const fileExist = yield fileDirExists(cfgPath)
+module.exports = function (cfgPath = 'publishcfg\\config.json') {
+    co(function* () {
+        const fileExist = yield fileDirExists(cfgPath)
 
-		if (!fileExist) {
-			console.log(
-				chalk.red(
-					'\n发布失败，原因：请在项目根目录执行当前命令，并在config/config.json里面配置了server服务器信息。'
-				)
-			)
-			process.exit()
-		}
-		try {
-			const cfg = JSON.parse(yield readFile(cfgPath))
-			cfg.servers.forEach(item => {
-				revert(item, cfg.zipName)
-			});
-		} catch (error) {
-			console.log(chalk.red(error))
-		}
-	})
+        if (!fileExist) {
+            console.log(
+                chalk.red(
+                    '\n发布失败，原因：请在项目根目录执行当前命令，并在config/config.json里面配置了server服务器信息。'
+                )
+            )
+            process.exit()
+        }
+        try {
+            const cfg = JSON.parse(yield readFile(cfgPath))
+            cfg.servers.forEach(item => {
+                revert(item, cfg.zipName)
+            });
+        } catch (error) {
+            console.log(chalk.red(error))
+        }
+    })
 }
